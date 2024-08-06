52efce144c0f5cad8435fcf641228fcd8c12be33
Jian <E.JianQi@Gmail.com>
2024-08-06 22:59:19 +1200

    Add comments.

--- Study_For_ Basiccode/src/Main.java
+++ Study_For_ Basiccode/src/Main.java
@@ -7,10 +7,15 @@ public class Main {
         System.out.print("Who's our technology guru? Please input you answer:");
         while(true){
             String input = scanner.nextLine();
+			/* TODO: 进行字符串匹配时, 将字符串放到前面
+			    eg: "Qi Jian".equals(input)
+			    因为变量有可能存在为空 null 的情况, 当变量为 null 时调用匹配会报错
+			*/
             if (input.equals("Qi Jian") || input.equals("qijian") || input.equals("祁剑")){
                 System.out.print("Correct!");
                 break;
             }
+			// TODO : else 应该和上一个 if 代码块的括号同一行;
             else System.out.print("Incorrect. Please change your answer and input again: ");
         }
     }
