8b07735847d7f1f4cbe009fd0100611d2ae221da
lucas <lijie.lucas@hotmail.com>
2024-08-06 22:46:18 +1200

    test for while

--- Study_For_ Basiccode/src/Main.java
+++ Study_For_ Basiccode/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i=1;i<=5;i++){
-            System.out.println(i);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Who's our technology guru? Please input you answer:");
+        while(true){
+            String input = scanner.nextLine();
+            if (input.equals("Qi Jian") || input.equals("qijian") || input.equals("祁剑")){
+                System.out.print("Correct!");
+                break;
+            }
+            else System.out.print("Incorrect. Please change your answer and input again: ");
         }
     }
 }
