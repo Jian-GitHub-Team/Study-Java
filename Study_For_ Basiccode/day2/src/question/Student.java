194db47c8bff5fae91f73a5006bf3e28169c8379
Jian <E.JianQi@Gmail.com>
2024-12-02 22:28:13 +1300

    Fix Bug

--- Study_For_ Basiccode/day2/src/question/Student.java
+++ Study_For_ Basiccode/day2/src/question/Student.java
@@ -1,10 +1,12 @@
 package question;
 
-public class student {
-    Teacher[] teacher = new Teacher[10];
-    String name;
-    int age;
-    char gender;
+public class Student {
+	Teacher[] teacher = new Teacher[10];
+	String name;
+	int age;
+	char gender;
 
-    teacher[0] = new Teacher {"tom","math", 12};
+	public Student() {
+		teacher[0] = new Teacher("tom", "math", 12);
+	}
 }
