3b46a4323998ffca1facf8a1ef0e38f107e52651
Tom <tyohin2016@163.com>
2024-12-02 22:25:13 +1300

    双色球  bug更新

---
+++ Study_For_ Basiccode/day2/src/question/Student.java
@@ -0,0 +1,10 @@
+package question;
+
+public class student {
+    Teacher[] teacher = new Teacher[10];
+    String name;
+    int age;
+    char gender;
+
+    teacher[0] = new Teacher {"tom","math", 12};
+}
---
+++ Study_For_ Basiccode/day2/src/question/Teacher.java
@@ -0,0 +1,16 @@
+package question;
+
+public class Teacher {
+    String name;
+    String subject;
+    int aga;
+
+    public Teacher() {
+    }
+    public Teacher(String name, String subject, int aga) {
+        this.name = name;
+        this.subject = subject;
+        this.aga = aga;
+
+    }
+}
